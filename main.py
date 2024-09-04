79cb1c260cd051b8d5c7e1aaf450a7e5fb04ba1c
UchithmaSenevirathne <uchithma@gmail.com>
2024-09-04 12:17:41 +0530

    variables, if else, while loop, string concact, nested

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
 print("Hello, World!")
+
+# if- else-elseif
+if 5 > 6:
+  print("if true")
+elif 4 < 5:
+  print("else if true")
+else:
+  print("False")
+ 
+# concact 
+x = "Uchi"
+y = 22
+print('age : ', y)
+
+# give data type
+x = str(3)
+y = int(3)
+z = float(3)
+
+print('string : ', x, ', int : ', y, ' ,float : ', z)
+
+# nested
+if 2 < 3:
+    print("2")
+    if 2 < 0:
+        print("0")
+    else:
+        print("null")
+
+# while loop
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
