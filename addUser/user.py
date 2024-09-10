3fc7d425845a84b8552c615b0de6d3db02f320d3
UchithmaSenevirathne <uchithma@gmail.com>
2024-09-10 19:55:49 +0530

    create user class to use objects and create function for get inputs and save db

--- addUser/user.py
+++ addUser/user.py
@@ -11,19 +11,38 @@ db = mysql.connector.connect(
 #create a cursor object for interacting with a MySQL database through the mysql.connector module.
 cursor = db.cursor()
 
-#get input values
-userName = input("Enter your Email : ")
-password = input("Enter your Password : ")
+#User class store userName and password
+class User:
+  def __init__(self, userName, password):
+    self.userName = userName
+    self.password = password
 
-#insert user data into database
-try:
-    query = "INSERT INTO users (userName, password) VALUES (%s, %s)"
-    cursor.execute(query, (userName, password))
-    db.commit()
-    print("User Added Successfully !")
-except mysql.connector.Error as err:
-    print(f"Error: {err}")
-finally:
-    # close connection and cursor
-    cursor.close()
-    db.close()
+def getUserInputs():
+    #get input values
+    userName = input("Enter your Email : ")
+    password = input("Enter your Password : ")
+    #return User object
+    return User(userName, password)
+     
+
+def saveUser():
+    #get user input as an user object
+    user = getUserInputs()
+    
+    print(user.userName)
+    print(user.password)
+    
+    #insert user data into database
+    try:
+        query = "INSERT INTO users (userName, password) VALUES (%s, %s)"
+        cursor.execute(query, (user.userName, user.password))
+        db.commit()
+        print("User Added Successfully !")
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+    finally:
+        # close connection and cursor
+        cursor.close()
+        db.close()
+        
+saveUser()
